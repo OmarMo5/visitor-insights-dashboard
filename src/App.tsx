232d1c8dd6ee3238fc53062454a3e4699acdd0e5
OmarMo5 <omarmo5tar12@gmail.com>
2026-09-22 11:21:14 +0300

    update config to run on server

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Visitor">
+      <BrowserRouter basename="/visitor">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route
